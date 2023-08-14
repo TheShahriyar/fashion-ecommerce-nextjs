f7bcdc153e0062763aaac8ec81738667252c2edf
SHAHRIAR.AHMED <shahriar.ahmed@solislab.com>
2023-08-14 17:54:27 +0600

    initial setup

---
+++ app/fonts.ts
@@ -0,0 +1,13 @@
+import { Jost, Open_Sans } from "next/font/google";
+
+export const jost = Jost({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-jost",
+});
+
+export const openSans = Open_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-open-sans",
+});
