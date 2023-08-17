440d53bd75d5826fb491b2f05c5e3f35a3b7bf94
TheShahriyar <shahriyar.31dec@gmail.com>
2023-08-17 19:20:10 +0600

    single product data show

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getSingleProduct } from "@/utilities/products";
+import Image from "next/image";
 import React from "react";
 
 const ProductDetails = async ({ params }: any) => {
@@ -9,9 +10,50 @@ const ProductDetails = async ({ params }: any) => {
   console.log(product);
 
   return (
-    <div>
-      <h1>{product.title}</h1>
-    </div>
+    <>
+      <section>
+        <div className="container">
+          <div className="flex space-x-20">
+            <div className="w-1/2 grid grid-cols-3 gap-4">
+              <div>
+                {product.images.map((image: any, index: any): any => (
+                  <Image
+                    key={index}
+                    src={image}
+                    alt={product.title}
+                    width={0}
+                    height={0}
+                    quality={100}
+                    style={{ width: "100%", height: "auto" }}
+                  />
+                ))}
+              </div>
+              <Image
+                src={product.thumbnail}
+                alt={product.title}
+                width={0}
+                height={0}
+                quality={100}
+                style={{ width: "100%", height: "auto" }}
+              />
+            </div>
+            <div className="w-1/2">
+              <h1>{product.title}</h1>
+              <div>Old Price: {product.price}</div>
+              <div>
+                New Price:{" "}
+                {product.price -
+                  product.price * (product.discountPercentage / 100)}
+              </div>
+              <div>Rating: {product.rating}</div>
+              <div>Brand: {product.brand}</div>
+              <div>Category: {product.category}</div>
+              <p>{product.description}</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
